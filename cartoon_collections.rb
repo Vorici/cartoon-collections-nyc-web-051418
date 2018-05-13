cf3cb62f33f46db9e262ef3694e6ef375ee05b2b
Ledio Bode <l.bode515@gmail.com>
2018-05-13 21:59:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_hash = Hash.new
+  dwarf_names.each_with_index {|dwarf|
+    print hash[dwarf] = index
+  end
+  hash
 end
 
 def summon_captain_planet# code an argument here
